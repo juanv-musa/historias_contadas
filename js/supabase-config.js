d883528a782601e88b93e30e7d3c54c888f44324
juanv-musa <jvargascorral@gmail.com>
2026-04-15 14:37:44 +0200

    Add files via upload

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -3,6 +3,6 @@
 // <script src="https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2"></script>
 
 export const SUPABASE_URL = "https://ptbciswzgeczryxjwvag.supabase.co";
-export const SUPABASE_KEY = "sb_publishable_51Njj_gV96OyHDCvOY7muQ_0vFM_gnH";
+export const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB0YmNpc3d6Z2VjenJ5eGp3dmFnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYyMDI5ODMsImV4cCI6MjA5MTc3ODk4M30.TbM5C6950Si7qdEfQlD6fU9NPkLzJ1gyU6xEhn7tbRc";
 
 export const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
